5eaabc2970e2a30da966f01e77c8d3899f66b75c
OperationChangeUddevalla <operationchangeuddevalla@gmail.com>
2024-10-31 14:27:01 +0100

    deployment ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.operationchange-uddevalla.org",
-	integrations: [tailwind(), sitemap(
-		{
-			filter: (_page) => false
-		}
-	)]
+	integrations: [tailwind(), sitemap()]
 });
