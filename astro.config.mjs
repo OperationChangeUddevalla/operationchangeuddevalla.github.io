9d3a74355f242e8760f79d96f2b2cc9a3054294b
OperationChangeUddevalla <operationchangeuddevalla@gmail.com>
2025-01-15 12:42:44 +0100

    trip, astro domain config, swish

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.operationchange-uddevalla.org",
+	site: "https://www.sugaid.org",
 	integrations: [tailwind(), sitemap()]
 });
